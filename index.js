205b98efd82b83e6f2cc390dfc664dfbcf143260
Hubert Wang <hubert_wang@trend.com.tw>
2018-02-02 16:35:53 +0800

    initialize

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-'use strict'
+var linebot = require('linebot');
+ 
+var CHANNEL_ID = null;
+var CHANNEL_SECRET = "";
+var CHANNEL_ACCESS_TOKEN = ""
 
-var express = require('express')
-var app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-var server = app.listen(8000, function () {
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('hello! http://%s:%s', host, port)
-})
+var bot = linebot({
+  channelId: CHANNEL_ID,
+  channelSecret: CHANNEL_SECRET,
+  channelAccessToken: CHANNEL_ACCESS_TOKEN,
+});
+ 
+bot.on('message', function (event) {
+  console.log('Event:', JSON.stringify(event));
+  let userId = event.source.userId;
+  if (userId) {
+    bot.push(userId, event.message.text).then(function (data) {
+      // success
+    }).catch(function (error) {
+      console.log('Error: ' + JSON.stringify(error));
+    });
+  }
+});
+ 
+bot.listen('/', 3000);
