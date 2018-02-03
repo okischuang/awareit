bd162d5ff3778818554145ad0d9746a88092efab
Hubert Wang <hubert_wang@trend.com.tw>
2018-02-03 10:42:03 +0800

    [Message] Add templates

---
+++ constants.js
@@ -0,0 +1,11 @@
+
+'use strict';
+
+module.exports = {
+  ACTION: {
+    ADD_NEW_THING: 'ADD_NEW_THING',
+    ADD_THING_PLACE: 'ADD_THING_PLACE',
+    ADD_THING_TAGS: 'ADD_THING_TAGS',
+    EDIT_THINGS: 'EDIT_THINGS',
+  },
+}
